fe37a1b1a84043a21f2d6f340410e992711c859f
Florian Privé <florian.prive.21@gmail.com>
2018-03-27 09:47:02 +0200

    add travis

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -2,6 +2,6 @@
 # Generator token: 10BE3573-1514-4C36-9D1C-5A225CD40393
 
 timesTwo <- function(x) {
-    .Call('_minidplyr_timesTwo', PACKAGE = 'minidplyr', x)
+    .Call(`_minidplyr_timesTwo`, x)
 }
 
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+[![Travis build status](https://travis-ci.org/privefl/minidplyr.svg?branch=master)](https://travis-ci.org/privefl/minidplyr)
+
 # minidplyr
